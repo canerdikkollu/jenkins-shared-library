0610cdeded8fae19cc08b73c95cc0b5d267b9120
caner <cdikkollu@gmail.com>
2022-06-08 00:10:17 +0300

    bugfix library

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,10 +3,7 @@
 def call(String APP_IMAGE_REGISTRY, String APP_IMAGE_REPOSITORY) {
 
     dir("${WORKSPACE}") {
-        sh "echo test"
         sh "docker build -t ${APP_IMAGE_REGISTRY}/${APP_IMAGE_REPOSITORY}:\$(git rev-parse HEAD) ."
-    //   sh "echo $DOCKERHUB_CRED_PSW | docker login -u $DOCKERHUB_CRED_USR --password-stdin"
-    //   sh "docker push ${APP_IMAGE_REGISTRY}/${APP_IMAGE_REPOSITORY}:\$(git rev-parse HEAD)"
     }
 
 }
